d4aa44ab80d22d0ca560e902ced4c6fc567543ce
Georgiy95 <urijpetrovskij@gmail.com>
2023-06-19 12:20:29 +0300

    commit

--- js/task-01.js
+++ js/task-01.js
@@ -1,15 +1,11 @@
 "use strict";
 
-const categories = document.querySelector('#categories');
-const items = categories.querySelectorAll('.item');
-console.log(`Number of categories: ${items.length}
+const categoriesEl = document.querySelectorAll('.item');
+console.log(`Number of categories: ${categoriesEl.length}
 
 `);
-items.forEach(item =>{
-const title = item.querySelector('h2').textContent;
-const elements = item.querySelectorAll('li').length;
-console.log(`Category: ${title}
-Elements: ${elements}
-
-`);
+categoriesEl.forEach(item => {
+    console.log(`Category: ${item.firstElementChild.textContent}`);
+    console.log(`Elements: ${item.lastElementChild.children.length}
+ `);
 });
